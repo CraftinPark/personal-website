286787da0e22f1c574b60e8be12bcb133f6174e5
CraftinPark <craftinpark@gmail.com>
2022-08-29 19:24:39 -0700

    working router

--- server/index.js
+++ server/index.js
@@ -2,6 +2,31 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 9000;
 const path = require("path");
+const mongoose = require("mongoose");
+
+const User = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  memberList: {
+    type: [
+      {
+        id: String,
+        name: String,
+        kName: String,
+        year: Number,
+        sex: String,
+        leader: Boolean,
+        active: Boolean,
+      },
+    ],
+  },
+});
+
+const model = mongoose.model("UserData", User);
+
+mongoose.connect(
+  "mongodb+srv://admin:Itrinity3@craftinpark.nrllh0g.mongodb.net/?retryWrites=true&w=majority"
+);
 
 app.use(
   "/home",
@@ -12,12 +37,43 @@ app.use(
   express.static(path.join(__dirname, "../dist/jomaker-build"))
 );
 
+// personal website
+
 app.get("home/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/../dist/personal-website-build/index.html"));
+  res.sendFile(
+    path.join(__dirname, "/../dist/personal-website-build/index.html")
+  );
 });
+
+// jomaker
 
 app.get("/jomaker/*", (req, res) => {
   res.sendFile(path.join(__dirname, "/../dist/jomaker-build/index.html"));
+});
+
+app.post("/jomaker/register", async (req, res) => {
+  console.log(req.body);
+  try {
+    const user = await User.create({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json({ status: "ok" });
+  } catch (err) {
+    res.json({ status: "error", error: "duplicate email" });
+  }
+});
+
+app.post("/jomaker/login", async (req, res) => {
+  const user = await User.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  if (user) {
+    res.json({ status: "ok" });
+  } else {
+    return res.json({ status: "error", user: false });
+  }
 });
 
 app.get("/test", (req, res) => {
