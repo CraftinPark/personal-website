b8872690c276c3c58200ae5c509c77f4582de12b
CraftinPark <craftinpark@gmail.com>
2022-10-29 22:32:05 -0700

    update jomaker to 1.1.7

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -1,14 +1,18 @@
 import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
       <p>
         This site is under construction! If you want to use Jo Maker, click the
         button below!
       </p>
-      <a href={'http://www.craftinpark.ca/jomaker/'}>to Jo Maker</a>
+      <a
+        href={"http://www.craftinpark.ca/jomaker/"}
+        style={{ fontSize: "30px" }}
+      >
+        to Jo Maker
+      </a>
     </div>
   );
 }
